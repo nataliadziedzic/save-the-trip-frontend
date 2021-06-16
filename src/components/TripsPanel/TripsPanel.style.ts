b783ef7c5b3141fbdc585caa0795e53fcb7d27a7
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-06-16 12:49:06 +0200

    Add styles to trip panel - display trips data

---
+++ src/components/TripsPanel/TripsPanel.style.ts
@@ -0,0 +1,76 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { SIZES } from '../../assets/styles/mediaQueries'
+
+export const TripsContainer = styled.section`
+  width: 90%;
+  max-width: 1200px;
+  margin: 42px auto 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .heading {
+    text-transform: uppercase;
+    margin-bottom: 32px;
+  }
+`
+export const Trip = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  row-gap: 8px;
+  align-items: center;
+  justify-items: center;
+  border-bottom: 1px solid #fff;
+  border-radius: 10px;
+  padding: 8px 24px 4px;
+  margin-bottom: 32px;
+  .tripDescription,
+  .tripStart {
+    justify-self: center;
+  }
+  .text {
+    font-size: 14px;
+  }
+  .boldText {
+    font-weight: 500;
+  }
+  @media only screen and ${SIZES.tablet} {
+    grid-template-columns: 1fr 1fr 1fr 100px;
+    align-items: center;
+    justify-items: start;
+  }
+`
+export const StyledLink = styled(Link)`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  padding: 8px;
+  overflow: hidden;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: -150%;
+    width: 150%;
+    height: 40%;
+    background-color: #fff;
+    opacity: 0.2;
+    transform: rotate(45deg);
+    transition: all ease 1.3s;
+  }
+  &:hover:before {
+    left: 100%;
+  }
+  .MuiSvgIcon-root {
+    margin-left: 8px;
+    font-size: 1.1rem;
+  }
+  @media only screen and ${SIZES.tablet} {
+    justify-self: end;
+  }
+`
--- src/components/TripsPanel/TripsPanel.tsx
+++ src/components/TripsPanel/TripsPanel.tsx
@@ -1,25 +1,57 @@
 import * as React from 'react'
-import { getTrips } from '../../api/trips.api'
+import { format } from 'date-fns'
+import { useIntl } from 'react-intl'
 import { useAppSelector } from '../../redux/hooks'
 import { UserTrips } from '../../types'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
+import { getTrips } from '../../api/trips.api'
+import { TripsContainer, Trip, StyledLink } from './TripsPanel.style'
 
 export interface TripsPanelProps {}
 
 const TripsPanel: React.FC<TripsPanelProps> = () => {
+  const intl = useIntl()
   const user = useAppSelector(state => state.user)
   const [trips, setTrips] = React.useState<UserTrips | []>([])
+  const [tripsDates, setTripsDates] = React.useState<string[]>([])
 
   React.useEffect(() => {
     getTrips(user.id!, setTrips)
   }, [user.id])
 
+  React.useEffect(() => {
+    setTripsDates([])
+    if (trips.length > 0) {
+      trips.forEach(trip => {
+        const startDate = new Date(trip.start_date!)
+        setTripsDates(state => [...state, format(startDate, 'dd-MM-yyyy')])
+      })
+    }
+  }, [trips])
+
   return (
-    <div>
-      PANEL {user.username}
-      {trips.map(trip => (
-        <div key={trip.id}>TRIP: {trip.title}</div>
+    <TripsContainer>
+      <h1 className='heading'>{intl.formatMessage({ id: 'heading-my-trips' })}</h1>
+      {trips.map((trip, index) => (
+        <Trip key={trip.id}>
+          <div className='tripTitle text'>
+            <span className='boldText'>{intl.formatMessage({ id: 'title' })}</span>{' '}
+            {trip.title?.length! > 20 ? trip.title?.slice(0, 20)! + '...' : trip.title}
+          </div>
+          <div className='tripDescription text'>
+            <span className='boldText'>{intl.formatMessage({ id: 'description' })}</span>{' '}
+            {trip.description?.length! > 20 ? trip.description?.slice(0, 20)! + '...' : trip.description}
+          </div>
+          <div className='tripStart text'>
+            <span className='boldText'>Start:</span> {tripsDates[index]}
+          </div>
+          <StyledLink to={`/trip/${trip.title}/${trip.id}`}>
+            <span>{intl.formatMessage({ id: 'see' })}</span>
+            <ArrowForwardIosIcon />
+          </StyledLink>
+        </Trip>
       ))}
-    </div>
+    </TripsContainer>
   )
 }
 
